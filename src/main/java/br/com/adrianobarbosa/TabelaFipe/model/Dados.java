e723c85ed1ea2b0627e22c0442ae5be59c8471a3
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-01-14 11:25:43 -0300

    commit final

---
+++ src/main/java/br/com/adrianobarbosa/TabelaFipe/model/Dados.java
@@ -0,0 +1,4 @@
+package br.com.adrianobarbosa.TabelaFipe.model;
+
+public record Dados (String codigo, String nome){
+}
--- src/main/java/br/com/adrianobarbosa/TabelaFipe/service/IConverteDados.java
+++ src/main/java/br/com/adrianobarbosa/TabelaFipe/service/IConverteDados.java
@@ -1,5 +1,10 @@
 package br.com.adrianobarbosa.TabelaFipe.service;
 
+import java.util.List;
+
 public interface IConverteDados {
+
     <T> T obterDados(String json, Class<T> classe);
+
+    <T> List<T> obterLista(String json, Class<T> classe);
 }
